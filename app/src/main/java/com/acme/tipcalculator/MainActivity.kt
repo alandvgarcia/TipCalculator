1608f8f98dfb735c2af7030444f6394ecde94ac4
Alan D V Garcia <alandvgarcia@gmail.com>
2018-08-13 22:40:40 -0300

    implementado databing no layout main e content e observable no viewmodel

--- app/src/main/java/com/acme/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/acme/tipcalculator/MainActivity.kt
@@ -1,39 +1,24 @@
 package com.acme.tipcalculator
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-
-import kotlinx.android.synthetic.main.activity_main.*
+import com.acme.tipcalculator.databinding.ActivityMainBinding
+import com.acme.tipcalculator.viewmodel.CalculatorViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.vm = CalculatorViewModel()
+
+        setSupportActionBar(binding.toolbar)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
--- app/src/main/java/com/acme/tipcalculator/viewmodel/CalculatorViewModel.kt
+++ app/src/main/java/com/acme/tipcalculator/viewmodel/CalculatorViewModel.kt
@@ -1,22 +1,35 @@
 package com.acme.tipcalculator.viewmodel
 
-import android.arch.lifecycle.ViewModel
+import android.databinding.BaseObservable
+import android.databinding.ObservableField
+import android.util.Log
 import com.acme.tipcalculator.model.RestaurantCalculator
 import com.acme.tipcalculator.model.TipCalculation
 
-class CalculatorViewModel(val calculator: RestaurantCalculator = RestaurantCalculator()) {
+class CalculatorViewModel(val calculator: RestaurantCalculator = RestaurantCalculator()) : BaseObservable() {
 
-    var inputCheckAmount = ""
-    var inputTipPercentage = ""
+    val TAG = "CalculatorViewModel"
+    var inputCheckAmount = ObservableField<String>("")
+    var inputTipPercentage = ObservableField<String>("")
     var tipCalculation = TipCalculation()
 
     fun calculateTip() {
-        val checkAmount = inputCheckAmount.toDoubleOrNull()
-        val tipPct = inputTipPercentage.toIntOrNull()
+        Log.d(TAG, "calculateTipInvoked")
+
+        val checkAmount = inputCheckAmount.get()?.toDoubleOrNull()
+        val tipPct = inputTipPercentage.get()?.toIntOrNull()
 
         if (checkAmount != null && tipPct != null) {
+            Log.d(TAG, "CheckAmount $checkAmount, TipPercentage $tipPct")
             tipCalculation = calculator.calculateTip(checkAmount, tipPct)
+            clearInputs()
         }
+    }
+
+    fun clearInputs() {
+        inputCheckAmount.set("0.00")
+        inputTipPercentage.set("0")
+        notifyChange()
     }
 
 
